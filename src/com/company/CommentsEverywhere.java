7fd790055d6fa820e9c75ea42379ea6cbaf5442b
GBTC406001UR <GBTC406001UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-23 14:40:20 -0400

    Initial Commit

---
+++ src/com/company/CommentsEverywhere.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class CommentsEverywhere {
+/*
+    Comments Example Program
+    Dave Wolf
+    July 21st, 2015
+
+ */
+    public static void main(String[] args) {
+        //This line will be ignores by the compiler
+        //but people will (hoepfully) read it.
+        //Anything after the // is ignored by Java.
+        System.out.println("This line prints just fine");
+        //System.out.println("This line wont print");
+        System.out.println("This line also prints jsut fine.");
+    }
+}
